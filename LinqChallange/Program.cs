9f858fce4fa35fcf6dbd482cd52deb4da2d1a618
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-01 20:40:15 +0200

    Basic Goal
    Added logic and console ui for basic goal of LINQ challange

--- LinqChallange/Program.cs
+++ LinqChallange/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Write word to process: ");
+
+string word = Console.ReadLine();
+
+var wordlist = from letter in word.ToCharArray()
+               orderby letter.ToString().ToLower() ascending
+               select letter;
+
+wordlist.ToList().ForEach(x => Console.Write(x));
