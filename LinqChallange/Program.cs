d144b98474cc7760f1a8d9356463076a4a0f3b7b
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-01 20:58:29 +0200

    Bonus goal
    Added logic for bonus LINQ challange

--- LinqChallange/Program.cs
+++ LinqChallange/Program.cs
@@ -2,8 +2,18 @@
 
 string word = Console.ReadLine();
 
-var wordlist = from letter in word.ToCharArray()
-               orderby letter.ToString().ToLower() ascending
-               select letter;
+//var letterList = from x in word
+//               orderby x.ToString() ascending
+//               select x;
 
-wordlist.ToList().ForEach(x => Console.Write(x));
+//letterList.ToList().ForEach(x => Console.Write(x));
+
+var letterListAdv = from x in word
+                    group x by x.ToString().ToLower() into y
+                    orderby y.Count() descending, y.Key.ToString()
+                    select (new { Letter = y.Key, Count = y.Count() });
+
+foreach(var letter in letterListAdv)
+{
+    Console.WriteLine($"Letter: {letter.Letter}, Count: {letter.Count}");
+}
